a75878e5c6b63b7acbda2440dd5398d0a20bdde6
austin <doublehoon2@gmail.com>
2026-01-15 17:20:48 +0900

    authservice

--- src/main/java/com/example/discord/entity/User.java
+++ src/main/java/com/example/discord/entity/User.java
@@ -1,14 +1,17 @@
 package com.example.discord.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "users")
+@NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
@@ -19,5 +22,17 @@ public class User {
 
     private String password;
 
-    private String avatarUrl;
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @Builder
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+//    private String avatarUrl;
 }
---
+++ src/main/java/com/example/discord/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.example.discord.service;
+
+import com.example.discord.dto.register.SignupRequest;
+import com.example.discord.entity.User;
+import com.example.discord.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Long signup(SignupRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalStateException("EMAIL ALREADY_EXISTS");
+        }
+        if (userRepository.existsByNickname(request.getNickname())) {
+            throw new IllegalStateException("NICKNAME_ALREADY_EXISTS");
+        }
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .nickname(request.getNickname())
+                .build();
+
+        userRepository.save(user);
+        return user.getId();
+    }
+}
