158f4bfa87d31d7c78da6ee3643a37c1444cf3b7
austin <doublehoon2@gmail.com>
2026-01-15 17:22:59 +0900

    signup

--- src/main/java/com/example/discord/controller/AuthController.java
+++ src/main/java/com/example/discord/controller/AuthController.java
@@ -2,9 +2,11 @@ package com.example.discord.controller;
 
 import com.example.discord.dto.LoginRequest;
 import com.example.discord.dto.TokenResponse;
+import com.example.discord.dto.register.SignupRequest;
 import com.example.discord.entity.User;
 import com.example.discord.repository.UserRepository;
 import com.example.discord.security.JwtProvider;
+import com.example.discord.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +23,22 @@ public class AuthController {
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
 
+    private final AuthService authService;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new RuntimeException("USER_NOT_FOUND"));
 
         String token = jwtProvider.generateToken(user.getId());
+
+        return ResponseEntity.ok(new TokenResponse(token));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupRequest request) {
+        Long userId = authService.signup(request);
+        String token = jwtProvider.generateToken(userId);
 
         return ResponseEntity.ok(new TokenResponse(token));
     }
